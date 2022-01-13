176dcd321721320c0f127028d97382f9e9173fe1
Skymarce <81475820+Skymarce@users.noreply.github.com>
2022-01-13 23:35:18 +0300

    add to LS

--- dist/js/components/addToFavorites.js
+++ dist/js/components/addToFavorites.js
@@ -1,11 +1,24 @@
 function addToFavorites() {
     let buttonsToFavorites = document.querySelectorAll('.content__card-btns-like');
+    let favorites = [];
 
     buttonsToFavorites.forEach(item => {
         item.addEventListener('click', () => {
-            let img = item.parentElement.querySelector('.content__card-img-img').src;
-            let title = item.parentElement.querySelector('.content__card-title').textContent;
-            let price = item.parentElement.querySelector('.content__card-price').textContent;
+            let img = item.parentElement.parentElement.querySelector('.content__card-img-img').src;
+            let title = item.parentElement.parentElement.querySelector('.content__card-title').textContent;
+            let color = item.parentElement.parentElement.querySelector('.content__card-color').style.backgroundColor;
+            let price = item.parentElement.parentElement.querySelector('.content__card-price').textContent;
+            
+
+            let favoritesProducts = {
+                img: img,
+                title: title,
+                color: color,
+                price: price
+            }
+
+            favorites.push(favoritesProducts)
+            localStorage.setItem('Favorites', JSON.stringify(favorites));
         })
     })
 }
